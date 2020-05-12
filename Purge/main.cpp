d23e79d0060cf23e8b295934d64418134da20a8f
heape <okayrop2@yahoo.co.jp>
2020-05-12 21:28:10 +0900

    a

---
+++ Purge/main.cpp
@@ -0,0 +1,8 @@
+#include "includes.hpp"
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	PSTR lpCmdLine, INT nCmdShow)
+{
+	MessageBoxA(0, "HELLO GUYS!!!", "HELLO WORLD!!!", 0);
+	return 0;
+}
